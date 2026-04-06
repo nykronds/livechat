e1cac43bfff684c9f9974624e2f6676cb80449b5
nykronds <emynormanmusic@gmail.com>
2026-04-05 23:55:41 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCaPEZUjBGlUX0_99OqVLsPlxk-eSCL1XI",
+  authDomain: "synthlab-750bf.firebaseapp.com",
+  databaseURL: "https://synthlab-750bf-default-rtdb.firebaseio.com",
+  projectId: "synthlab-750bf",
+  storageBucket: "synthlab-750bf.firebasestorage.app",
+  messagingSenderId: "318338833192",
+  appId: "1:318338833192:web:67f46984d17e0984804e1d",
+  measurementId: "G-NJND6KMZ9T"
+};
